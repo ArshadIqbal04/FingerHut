5633b94b21e2109b4bd5c1825f1b65d1b6b92e79
Yatendra_Singh <yatendrasingh966@gmail.com>
2023-03-14 16:50:13 +0530

    Created HTML

---
+++ productsdetail.js
@@ -0,0 +1,38 @@
+var productData = [
+    {
+        image_url: ;
+        name: ;
+        emi: ;
+        price: ;
+    }
+    {
+        image_url: ;
+        name: ;
+        emi: ;
+        price: ;
+    }
+    {
+        image_url: ;
+        name: ;
+        emi: ;
+        price: ;
+    }
+    {
+        image_url: ;
+        name: ;
+        emi: ;
+        price: ;
+    }
+    {
+        image_url: ;
+        name: ;
+        emi: ;
+        price: ;
+    }
+    {
+        image_url: ;
+        name: ;
+        emi: ;
+        price: ;
+    }
+]
