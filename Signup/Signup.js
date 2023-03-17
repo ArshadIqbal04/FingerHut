87db45d28bccf63ad4ee08513e00626b0bd2a4a3
ArshadIqbal04 <arshana7602@gmail.com>
2023-03-17 21:28:27 +0530

    Signup and Login page created Succesfully

--- Signup/Signup.js
+++ Signup/Signup.js
@@ -1,14 +1,27 @@
-var arr = [];
-function signup(event) {
-    // alert("abcd")
-  event.preventDefault();
-  var data = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("mail").value,
-    password: document.getElementById("pass").value,
-    password: document.getElementById("confirmpass").value,
-  };
-  arr.push(data);
-  localStorage.setItem("signup", JSON.stringify(arr));
-  window.location.href = "../Login/Login.html";
+function signup(event){
+    event.preventDefault();
+
+    
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("mail").value;
+  const password = document.getElementById("pass").value;
+  const confirmPassword = document.getElementById("confirmpass").value;
+  const customerNumber = document.getElementById("customerNumber").value;
+  
+  if (name.trim() === "" || email.trim() === "" || password.trim() === "" || confirmPassword.trim() === "") {
+  alert("Please fill in all required fields.");
+  return;
+  }
+  if (password !== confirmPassword) {
+  alert("Passwords do not match. Please try again.");
+  return;
+  }
+  
+  // save the form data to local storage
+  const userData = { name, email, password, customerNumber };
+  localStorage.setItem("userData", JSON.stringify(userData));
+  
+  alert("Signup successful! You will be redirected shortly.");
+  window.location.replace("../Login/Login.html");
+    
 }
