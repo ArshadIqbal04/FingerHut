7fe044fdaf22c3b7d0f80d8094adcac27e028f13
Yatendra_Singh <yatendrasingh966@gmail.com>
2023-03-17 20:06:09 +0530

    Product Page

--- productsDisplay.js
+++ productsDisplay.js
@@ -42,6 +42,9 @@ document.getElementById("electronics").addEventListener("click",function(){
 document.getElementById("shoe").addEventListener("click",function(){
     itemsDisplay("Shoes")
 });
+document.getElementById("cloth").addEventListener("click",function(){
+    itemsDisplay("Cloth")
+});
 
 function itemsDisplay(cat){
     document.getElementById("productRegion-Card").textContent="";
@@ -77,6 +80,104 @@ function itemsDisplay(cat){
         }
     })
 }
+
+document.getElementById("puma").addEventListener("click",function(){
+    brandDisplay("Puma")
+});
+document.getElementById("reebook").addEventListener("click",function(){
+    brandDisplay("Reebook")
+});
+document.getElementById("new-balance").addEventListener("click",function(){
+    brandDisplay("New Balance")
+});
+document.getElementById("apple").addEventListener("click",function(){
+    brandDisplay("Apple")
+});
+document.getElementById("samsung").addEventListener("click",function(){
+    brandDisplay("Samsung")
+});
+
+function brandDisplay(cat){
+    document.getElementById("productRegion-Card").textContent="";
+    var count = 1;
+    dataArrls.map(function(elem,index){
+        if(elem.brand==cat && count<=16 ){
+            console.log("electronics")
+            count++;
+            var productCard = document.createElement("div");
+            
+            var img = document.createElement("img");
+            img.setAttribute("src",elem.img_url);
+            img.setAttribute("alt",elem.category);
+            console.log(img)
+
+            var name = document.createElement("div");
+            name.textContent=elem.name;
+            
+            var emi = document.createElement("p");
+            emi.textContent="$"+elem.emiPrice+" /month";
+            
+            var price = document.createElement("p");
+            price.textContent="$"+elem.price;
+            
+            var btn = document.createElement("button");
+            btn.textContent="Add to Cart";
+            // btn.addEventListener("click",function(){
+            //     addtocart(index);
+            // })
+
+            productCard.append(img,name,emi,price,btn);
+            document.getElementById("productRegion-Card").append(productCard);
+        }
+    })
+}
+
+document.getElementById("female").addEventListener("click",function(){
+    genderDisplay("Female")
+});
+document.getElementById("male").addEventListener("click",function(){
+    genderDisplay("Male")
+});
+
+function genderDisplay(cat){
+    document.getElementById("productRegion-Card").textContent="";
+    var count = 1;
+    dataArrls.map(function(elem,index){
+        if(elem.gender==cat && count<=16 ){
+            console.log("electronics")
+            count++;
+            var productCard = document.createElement("div");
+            
+            var img = document.createElement("img");
+            img.setAttribute("src",elem.img_url);
+            img.setAttribute("alt",elem.category);
+            console.log(img)
+
+            var name = document.createElement("div");
+            name.textContent=elem.name;
+            
+            var emi = document.createElement("p");
+            emi.textContent="$"+elem.emiPrice+" /month";
+            
+            var price = document.createElement("p");
+            price.textContent="$"+elem.price;
+            
+            var btn = document.createElement("button");
+            btn.textContent="Add to Cart";
+            // btn.addEventListener("click",function(){
+            //     addtocart(index);
+            // })
+
+            productCard.append(img,name,emi,price,btn);
+            document.getElementById("productRegion-Card").append(productCard);
+        }
+    })
+}
+
+// function sortingFun(){
+//     var sortingValue = document.getElementById("sortingbyPrice").value;
+//     console.log(sortingValue);
+// }
 
 function addtocart(index){
 
